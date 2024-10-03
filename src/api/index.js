e6078a05d6abf3a0844f527a9e8de52bbb18aaa2
cyberdevilz90 <ahmadmahmudi2000@gmail.com>
2024-10-03 18:54:42 +0700

    appwrite API

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,9 @@
 import { Client, Account } from "appwrite";
 
-const client = new Client()
-client.setEndpoint("https://cloud.appwrite.io/v1").setProject("66fca0ef0016b1bbf334")
+const client = new Client();
+client
+   .setEndpoint("https://cloud.appwrite.io/v1")
+   .setProject("66fca0ef0016b1bbf334");
 
-const account = new Account()
-export { client, account}
+const account = new Account(client);
+export { client, account };
