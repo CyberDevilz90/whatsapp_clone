4fd633de4a420b98451d1ae1ee5a791a39640b29
cyberdevilz90 <ahmadmahmudi2000@gmail.com>
2024-10-03 18:55:02 +0700

    Custom Component

---
+++ src/components/interface/CustomButton.vue
@@ -0,0 +1,16 @@
+<template>
+   <button
+     type="button"
+     class="inline-flex items-center justify-center px-3 py-2 text-sm font-medium leading-4 text-white bg-green-600 border border-transparent rounded-md shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+   >
+     {{ text }}
+   </button>
+ </template>
+ 
+ <script setup>
+ // eslint-disable-next-line
+ const props = defineProps({
+   text: String,
+ });
+ </script>
+ 
---
+++ src/components/interface/CustomInput.vue
@@ -0,0 +1,30 @@
+<template>
+   <div>
+     <label class="block text-sm font-medium text-gray-700">{{ label }}</label>
+     <div class="mt-1">
+       <input
+         :type="type"
+         class="block w-full border-gray-300 rounded-md shadow-sm focus:ring-green-500 focus:border-green-500 sm:text-sm"
+         :placeholder="placeholder"
+         v-model="model"
+         @input="$emit('update:modelValue', model)"
+       />
+     </div>
+   </div>
+ </template>
+ 
+ <script setup>
+ import { ref } from "vue";
+ // eslint-disable-next-line no-unused-vars
+ const props = defineProps({
+   label: String,
+   type: {
+     type: String,
+     required: true
+   },
+   placeholder: String
+ });
+ 
+ const model = ref();
+ </script>
+ 
